49770941b5cf3c574b685cba2b77dd8d0e6a7fa9
Dororo <hz86569p@pace.edu>
2021-09-27 21:11:00 -0400

    assignment02

---
+++ Assignment02/TabExperiment/app/src/main/java/com/example/tabexperiment/TabFragment1.java
@@ -0,0 +1,21 @@
+package com.example.tabexperiment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class TabFragment1 extends Fragment {
+
+    public TabFragment1() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment1, container, false);
+    }
+
+}
